484e193648b15f7e0a0c063fcde21034fcc76ed8
Dilyana Nikolaeva <d.nikolaeva.plovdiv@gmail.com>
2024-03-25 13:01:34 +0200

    Added tests for 'Add book' page

--- Automated Testing/Exe/Automated Testing - Exe/Library-Catalog/tests/ui.test.js
+++ Automated Testing/Exe/Automated Testing - Exe/Library-Catalog/tests/ui.test.js
@@ -13,10 +13,17 @@ const loginButtonSelector = 'a[href="/login"]';
 const logoutButtonSelector = '#logoutBtn';
 const registerButtonSelector = 'a[href="/register"]';
 const submitButtonSelector = 'input[type="submit"]';
+const createBookSubmitButtonSelector = '#create-form input[type="submit"]';
 
 const emailInputSelector = 'input[name="email"]';
 const passwordInputSelector = 'input[name="password"]';
 const repeatPasswordInputSelector = 'input[name="confirm-pass"]';
+
+const createFormSelector = '#create-form';
+const titleSelector = '#title';
+const descriptionSelector = '#description';
+const imageSelector = '#image';
+const typeSelector = '#type';
 
 //By default, dialogs are auto-dismissed by Playwright!!! 
 
@@ -156,7 +163,7 @@ test('Submit the form with empty password input field', async ({ page }) => {
 
 test('Register with valid credentials', async ({ page }) => {
     await page.goto(baseUrl + 'register');
-    await page.fill(emailInputSelector, 'registerUser@abv.bg');
+    await page.fill(emailInputSelector, 'testRegister@abv.bg');
     await page.fill(passwordInputSelector, password);
     await page.fill(repeatPasswordInputSelector, password);
     await page.click(submitButtonSelector);
@@ -245,7 +252,7 @@ test('Register with different password and confirm password input field', async 
     await page.goto(baseUrl + 'register');
     await page.fill(emailInputSelector, 'diffPass@abv.bg');
     await page.fill(passwordInputSelector, password);
-    await page.fill(repeatPasswordInputSelector,'456789');
+    await page.fill(repeatPasswordInputSelector, '456789');
     await page.click(submitButtonSelector);
 
     page.on('dialog', async dialog => {
@@ -257,4 +264,115 @@ test('Register with different password and confirm password input field', async 
     await page.$(loginButtonSelector);
 
     expect(page.url()).toBe(baseUrl + 'register');
+});
+
+// Add Book page tests
+
+test('Add book with valid data', async ({ page }) => {
+    await page.goto(baseUrl + 'login');
+
+    await page.fill(emailInputSelector, user);
+    await page.fill(passwordInputSelector, password);
+
+    await Promise.all([
+        page.click(submitButtonSelector),
+        page.waitForURL(baseUrl + 'catalog')
+    ]);
+
+    await page.click(addBookLinkSelector);
+    await page.waitForSelector(createFormSelector);
+    await page.fill(titleSelector, 'Test Book');
+    await page.fill(descriptionSelector, 'Test book description');
+    await page.fill(imageSelector, 'https://shorturl.at/cjmy0');
+    await page.selectOption(typeSelector, 'Other');
+
+    await page.click(createBookSubmitButtonSelector);
+
+    await page.waitForURL(baseUrl + 'catalog');
+
+    expect(page.url()).toBe(baseUrl + 'catalog');
+});
+
+test('Add book with empty title feld', async ({ page }) => {
+    await page.goto(baseUrl + 'login');
+
+    await page.fill(emailInputSelector, user);
+    await page.fill(passwordInputSelector, password);
+
+    await Promise.all([
+        page.click(submitButtonSelector),
+        page.waitForURL(baseUrl + 'catalog')
+    ]);
+
+    await page.click(addBookLinkSelector);
+    await page.waitForSelector(createFormSelector);
+    await page.fill(descriptionSelector, 'Test book description');
+    await page.fill(imageSelector, 'https://shorturl.at/cjmy0');
+    await page.selectOption(typeSelector, 'Fiction');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$(addBookLinkSelector);
+
+    expect(page.url()).toBe(baseUrl + 'create');
+});
+
+test('Add book with empty description feld', async ({ page }) => {
+    await page.goto(baseUrl + 'login');
+
+    await page.fill(emailInputSelector, user);
+    await page.fill(passwordInputSelector, password);
+
+    await Promise.all([
+        page.click(submitButtonSelector),
+        page.waitForURL(baseUrl + 'catalog')
+    ]);
+
+    await page.click(addBookLinkSelector);
+    await page.waitForSelector(createFormSelector);
+    await page.fill(titleSelector, 'Test Book');
+    await page.fill(imageSelector, 'https://shorturl.at/cjmy0');
+    await page.selectOption(typeSelector, 'Mistery');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$(addBookLinkSelector);
+
+    expect(page.url()).toBe(baseUrl + 'create');
+});
+
+test('Add book with empty image feld', async ({ page }) => {
+    await page.goto(baseUrl + 'login');
+
+    await page.fill(emailInputSelector, user);
+    await page.fill(passwordInputSelector, password);
+
+    await Promise.all([
+        page.click(submitButtonSelector),
+        page.waitForURL(baseUrl + 'catalog')
+    ]);
+
+    await page.click(addBookLinkSelector);
+    await page.waitForSelector(createFormSelector);
+    await page.fill(titleSelector, 'Test Book');
+    await page.fill(descriptionSelector, 'Test book description');
+    await page.selectOption(typeSelector, 'Mistery');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$(addBookLinkSelector);
+
+    expect(page.url()).toBe(baseUrl + 'create');
 });
